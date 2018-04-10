1f6831e661bd1c628009c6b05e081002642177aa
texnixe <info@texniq.de>
2018-04-10 13:49:15 +0200

    Check if file exists and has the right mime type

--- tags/pdf.php
+++ tags/pdf.php
@@ -7,34 +7,38 @@
  * license: http://www.gnu.org/licenses/gpl-3.0.txt GPLv3 License
  */
 
-kirbytext::$tags['pdf'] = array(
+kirbytext::$tags['pdf'] = [
   'attr' => array(
     'class'
   ),
   'html' => function($tag) {
-    $fileUrl = $tag->page()->contentUrl() . '/' . $tag->attr('pdf');
-    $class = $tag->attr('class', 'pdf');
+    $file = $tag->file($tag->attr('pdf'));
+    $object = '';
+    if($file && $file->mime() === 'application/pdf') {
+      $fileUrl = $file->url();
+      $class = $tag->attr('class', 'pdf');
 
-    $object = brick('object');
-    $object->addClass($class);
-    $object->attr('data', $fileUrl);
-    $object->attr('type', 'application/pdf');
+      $object = brick('object');
+      $object->addClass($class);
+      $object->attr('data', $fileUrl);
+      $object->attr('type', $file->mime());
 
-    $download = brick('a',
-      c::get('pdf.fallback_action', 'Download PDF'),
-      array(
-        'href' => $fileUrl,
-        'title' => c::get('pdf.fallback_action', 'Download PDF'),
-        'target' => '_blank'
-      )
-    );
+      $download = brick('a',
+        c::get('pdf.fallback_action', 'Download PDF'),
+        [
+          'href' => $fileUrl,
+          'title' => c::get('pdf.fallback_action', 'Download PDF'),
+          'target' => '_blank'
+        ]
+      );
 
-    $fallback = brick('p',
-      c::get('pdf.fallback', 'This browser doesn’t support PDFs. Please download the PDF to view it:'). ' ' . $download . '.'
-    );
+      $fallback = brick('p',
+        c::get('pdf.fallback', 'This browser doesn’t support PDFs. Please download the PDF to view it:'). ' ' . $download . '.'
+      );
 
-    $object->append($fallback);
+      $object->append($fallback);
 
-    return $object;
+      }
+      return $object;
   }
-);
+];
